04e62e27fca042312122d017b1cd75ca72f45fe8
patrickfung62 <patrickfung62@gmail.com>
2016-08-08 18:24:13 +0800

    haxe-test

---
+++ Test.hx
@@ -0,0 +1,12 @@
+class Test {
+  static function main() {
+    var people = [
+      "Elizabeth" => "Programming",
+      "Joel" => "Design"
+    ];
+    for (name in people.keys()) {
+      var job = people[name];
+      trace('$name does $job for a living!');
+    }
+  }
+}
